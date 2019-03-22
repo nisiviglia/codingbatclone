bbbfd016b8a2a300ce7246d2b9dcb751e010eb8c
nisiviglia <ns533@njit.edu>
2019-03-22 16:08:12 -0400

    first commit

---
+++ src/main/java/com/siviglia/web/codingbatclone/CodingbatcloneApplication.java
@@ -0,0 +1,13 @@
+package com.siviglia.web.codingbatclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingbatcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingbatcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siviglia/web/codingbatclone/CodingbatcloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siviglia.web.codingbatclone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodingbatcloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
