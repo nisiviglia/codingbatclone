b685f4d9539e1bb0ae274229eea00d2719e4aef4
nisiviglia <ns533@njit.edu>
2019-03-30 11:31:52 -0400

    Added framework for the admin page. It properly lists all questions and has a functioning prev and next button.

--- src/main/java/com/siviglia/web/codingbatclone/controller/IndexController.java
+++ src/main/java/com/siviglia/web/codingbatclone/controller/IndexController.java
@@ -6,11 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.siviglia.web.codingbatclone.repository.QuestionRepository;
 import com.siviglia.web.codingbatclone.model.Question;
-import com.siviglia.web.codingbatclone.model.QuestionTitle;
 import com.siviglia.web.codingbatclone.exception.NotFoundException;
 
 import java.util.List;
@@ -30,8 +32,18 @@ public class IndexController{
     @RequestMapping(value={"/", "/index"}, method= RequestMethod.GET)
     public String index(Model model){
         
-        List<Question> questions = questionRepository
-            .getFirst10ByVisibleIsTrue();
+        Page<Question> questions = questionRepository
+            .getAllByVisibleIsTrue(PageRequest.of(0, 10));
+
+        model.addAttribute("questions", questions);
+        return "index";
+    }
+
+    @RequestMapping(value={"/index/{page}"}, method= RequestMethod.GET)
+    public String indexPage(Model model, @PathVariable(value= "page") int page){
+        
+        Page<Question> questions = questionRepository
+            .getAllByVisibleIsTrue(PageRequest.of(page, 10));
 
         model.addAttribute("questions", questions);
         return "index";
@@ -42,5 +54,27 @@ public class IndexController{
         
         model.addAttribute("message", message);
         return "about";
+    }
+
+    @RequestMapping(value={"/admin"}, method= RequestMethod.GET)
+    public String admin(Model model){
+		
+		Page<Question> questions = questionRepository
+            .findAll( PageRequest.of(0, 10) );
+
+		model.addAttribute("questions", questions);        
+        model.addAttribute("page", 0);
+        return "admin";
+    }
+
+    @RequestMapping(value={"/admin/{page}"}, method= RequestMethod.GET)
+    public String adminPage(Model model, @PathVariable(value= "page") int page){
+
+		Page<Question> questions = questionRepository
+            .findAll( PageRequest.of(page, 10) );
+
+		model.addAttribute("questions", questions);        
+        model.addAttribute("page", page);
+        return "admin";
     }
 }
--- src/main/java/com/siviglia/web/codingbatclone/repository/QuestionRepository.java
+++ src/main/java/com/siviglia/web/codingbatclone/repository/QuestionRepository.java
@@ -1,9 +1,10 @@
 package com.siviglia.web.codingbatclone.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.siviglia.web.codingbatclone.model.Question;
-import com.siviglia.web.codingbatclone.model.QuestionTitle;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +13,7 @@ public interface QuestionRepository extends CrudRepository<Question, String>{
     
     public Optional<Question> findByTitleAndVisibleIsTrue(String title);    
 
-    public List<Question> getFirst10ByVisibleIsTrue();
+    public Page<Question> getAllByVisibleIsTrue(Pageable pageable);
+
+	public Page<Question> findAll(Pageable pageable);
 }
